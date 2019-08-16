60b7fe408370f7ac2757a954541cce06967aa60a
Your Name <you@example.com>
2019-08-16 08:33:34 -0700

    created reducers and store, wrapped App with ProvideR

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import {applyMiddleware, createStore} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {smurfReducer} from './reducers/smurfReducer'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+export const store = createStore(smurfReducer, applyMiddleware())
+console.log('store', store)
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>
+, document.getElementById("root"));
---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,13 @@
+
+const initialState = {
+
+}
+
+export const smurfReducer = (state, action) => {
+    switch(action.type) {
+
+
+        default:
+            return state
+    }
+}
