62132c771e415612dc897cea76a524a338116499
Your Name <you@example.com>
2019-08-16 09:58:09 -0700

    created actions for Posting

--- smurfs/src/actions/smurfAction.js
+++ smurfs/src/actions/smurfAction.js
@@ -5,6 +5,10 @@ export const FETCHING_SMURF_LOADING = 'FETCHING_SMURF_LOADING';
 export const FETCHING_SMURF_SUCCESS = 'FETCHING_SMURF_SUCCESS';
 export const FETCHING_SMURF_FAILURE = 'FETCHING_SMURF_FAILURE';
 
+export const POSTING_SMURF_LOADING = 'POSTING_SMURF_LOADING';
+export const POSTING_SMURF_SUCCESS = 'POSTING_SMURF_SUCCESS';
+export const POSTING_SMURF_FAILURE = 'POSTING_SMURF_FAILURE';
+
 
 export const getSmurfs = () => {
 
@@ -19,3 +23,15 @@ export const getSmurfs = () => {
     }
 }
 
+export const addSmurfs =() => {
+
+    return dispatch => {
+        dispatch({type: POSTING_SMURF_LOADING});
+        axios 
+            .post(`http://localhost:3333/smurfs`)
+            .then(
+                res => console.log('res in post', res)
+            )
+            .catch(err => console.log(err))
+    }
+}
